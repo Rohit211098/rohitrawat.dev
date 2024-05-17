d7ec61b20eba3d0331cd75b87d89ab64afa29b1f
Rohit Rawat <rohitrawat2198@gmail.com>
2024-05-17 22:42:56 +0530

    removed resume

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import { Container } from './styles'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { NavHashLink, HashLink } from 'react-router-hash-link'
 import { useState } from 'react'
-import Resume from '../../assets/Vinayak_Kumar_Singh_Resume.pdf'
+// import Resume from '../../assets/Vinayak_Kumar_Singh_Resume.pdf'
 export function Header() {
   const [isActive, setActive] = useState(false)
   function toggleTheme() {
@@ -40,9 +40,9 @@ export function Header() {
           <NavHashLink smooth to="#contact" onClick={closeMenu}>
             Contact
           </NavHashLink>
-          <a href={Resume} download className="button">
+          {/* <a href={Resume} download className="button">
             Resume
-          </a>
+          </a> */}
         </nav>
         <div
           aria-expanded={isActive ? 'true' : 'false'}
